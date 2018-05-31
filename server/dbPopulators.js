4f0278fc0362186f3d8739653a272001885b3eb6
Catherine Ligad <kateligad@gmail.com>
2018-05-31 14:24:20 -0700

    starting to populate the db with data using functions

---
+++ server/dbPopulators.js
@@ -0,0 +1,5 @@
+//INSERT INTO 'User_Listing' ('Name','User','Price_Per_Night','Star_Rating','Customer_Review_Num','Dates_Taken_ID','Min_Stay','Cleaning_Fee','Service_Fee') VALUES ('','','','','','','','','');
+
+const populateUserListingTable = () => {
+
+}
--- server/schema.sql
+++ server/schema.sql
@@ -1,53 +1,42 @@
+-- mysql -u root < server/schema.sql
 DROP DATABASE IF EXISTS `Booking`;
 
 CREATE DATABASE `Booking`;
 USE `Booking`;
-
-DROP TABLE IF EXISTS `User Listing`;
-DROP TABLE IF EXISTS `User`;
-DROP TABLE IF EXISTS `Dates Taken`;
-		
-CREATE TABLE `User Listing` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Name` VARCHAR(50) NULL DEFAULT NULL,
-  `User` INTEGER NULL DEFAULT NULL,
-  `Price_Per_Night` MEDIUMINT NULL DEFAULT NULL,
-  `Star_Rating` SMALLINT NULL DEFAULT NULL,
-  `Customer_Review_Num` SMALLINT NULL DEFAULT NULL,
-  `Dates_Taken_ID` INTEGER NULL DEFAULT NULL,
-  `Min_Stay` SMALLINT NULL DEFAULT NULL,
-  `Cleaning_Fee` SMALLINT NULL DEFAULT NULL,
-  `Service_Fee` SMALLINT NULL DEFAULT NULL,
+	
+CREATE TABLE `User_Listing` (
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
+  `Name` VARCHAR(50) NOT NULL,
+  `User` INTEGER NOT NULL,
+  `Price_Per_Night` MEDIUMINT NOT NULL,
+  `Star_Rating` SMALLINT NOT NULL,
+  `Customer_Review_Num` SMALLINT NOT NULL,
+  `Dates_Taken_ID` INTEGER NOT NULL,
+  `Min_Stay` SMALLINT NOT NULL,
+  `Cleaning_Fee` SMALLINT NOT NULL,
+  `Service_Fee` SMALLINT NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `User` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Name` VARCHAR(25) NULL DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
+  `Name` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`id`)
 );
 		
-CREATE TABLE `Dates Taken` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Year` INTEGER NULL DEFAULT NULL,
-  `Date` INTEGER NULL DEFAULT NULL,
+CREATE TABLE `Dates_Taken` (
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
+  `Year` YEAR NOT NULL,
+  `Date` DATE(370) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
--- ---
--- Foreign Keys 
--- ---
 
-ALTER TABLE `User Listing` ADD FOREIGN KEY (User) REFERENCES `User` (`id`);
-ALTER TABLE `User Listing` ADD FOREIGN KEY (Dates_Taken_ID) REFERENCES `Dates Taken` (`id`);
+--Foreign Keys 
+ALTER TABLE `User_Listing` ADD FOREIGN KEY (User) REFERENCES `User` (`id`);
+ALTER TABLE `User_Listing` ADD FOREIGN KEY (Dates_Taken_ID) REFERENCES `Dates_Taken` (`id`);
 
--- ---
--- Test Data
--- ---
 
--- INSERT INTO `User Listing` (`id`,`Name`,`User`,`Price_Per_Night`,`Star_Rating`,`Customer_Review_Num`,`Dates_Taken_ID`,`Min_Stay`,`Cleaning_Fee`,`Service_Fee`) VALUES
--- ('','','','','','','','','','');
--- INSERT INTO `User` (`id`,`Name`) VALUES
--- ('','');
--- INSERT INTO `Dates Taken` (`id`,`Year`,`Date`) VALUES
--- ('','','');
+-- INSERT INTO `User Listing` (`id`,`Name`,`User`,`Price_Per_Night`,`Star_Rating`,`Customer_Review_Num`,`Dates_Taken_ID`,`Min_Stay`,`Cleaning_Fee`,`Service_Fee`) VALUES ('','','','','','','','','','');
+-- INSERT INTO `User` (`id`,`Name`) VALUES ('','');
+-- INSERT INTO `Dates Taken` (`id`,`Year`,`Date`) VALUES ('','','');
