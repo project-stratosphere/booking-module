89c0bca0fffc9f6e6258aa484b0ca0e3102bc41b
Catherine Ligad <kateligad@gmail.com>
2018-05-31 18:26:09 -0700

    populated database

--- server/dbPopulators.js
+++ server/dbPopulators.js
@@ -1,17 +1,42 @@
-//INSERT INTO user (id,name) VALUES ('','');
-//Need to create 90 - 100 random users with IDs
+const faker = require('faker');
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+//INSERT INTO user (name) VALUES ('');
 const populateUsers = () => {
-
+  const arr = [];
+  for (let i = 0; i <= 100; i++) {
+    var randomName = faker.name.findName() + '");';
+    var string = 'INSERT INTO user (name) VALUES ("';
+    arr.push(string + randomName);
+  }
+  const result = arr.join('\n');
+  // console.log(result);
 };
+populateUsers();
 
-//INSERT INTO userListing (id,name,user_id,price_per_night,star_rating,cust_rev_num,min_stay,cleaning_fee,service_fee) VALUES ('','','','','','','','','');
-//Need to create 100 random user listings
+//INSERT INTO userListing (name,user_id,price_per_night,star_rating,cust_rev_num,min_stay,cleaning_fee,service_fee, max_guests) VALUES ('','','','','','','','','');
 const populateUserListingTable = () => {
-
+  const arr = [];
+  for (let i = 0; i <= 100; i++) {
+    var randomAddress = faker.address.streetAddress() + "',";
+    const user_id = getRandomIntInclusive(1, 100) + ',';
+    const price_per_night = getRandomIntInclusive(30, 600) + ',';
+    const star_rating = getRandomIntInclusive(1, 5) + ',';
+    const cust_rev_num = getRandomIntInclusive(20, 2000) + ',';
+    const min_stay = getRandomIntInclusive(1, 4) + ',';
+    const cleaning_fee = getRandomIntInclusive(20, 80) + ',';
+    const service_fee = getRandomIntInclusive(20, 50) + ',';
+    const max_guests = getRandomIntInclusive(1, 8) + ');';
+    var string = "INSERT INTO userListing (name,user_id,price_per_night,star_rating,cust_rev_num,min_stay,cleaning_fee,service_fee, max_guests) VALUES ('";
+    arr.push(string + randomAddress + user_id + price_per_night + star_rating + cust_rev_num + min_stay + cleaning_fee + service_fee + max_guests);
+  }
+  const result = arr.join('\n');
+  // console.log(result);
 };
+populateUserListingTable();
 
-//INSERT INTO occupied_dates (id,listing_id,date) VALUES ('','','');
-//Need to create many taken dates
-const populateDatesTaken = () => {
-
-};
