54458ad02576e0ce4942f495363da520f5566e0d
Catherine Ligad <kateligad@gmail.com>
2018-06-12 13:27:46 -0700

    use cors on the server

--- server/mysql.js
+++ server/mysql.js
@@ -1,10 +1,10 @@
-const mysql = require( 'mysql' );
+const mysql = require('mysql');
 
-const connection = mysql.createConnection( {
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'booking',
-} );
+});
 
 connection.connect();
 
